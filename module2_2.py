2e468ef90a0b4c4577ee724f8ad3cc3ae799fb30
Yampile <102218673+deqmos@users.noreply.github.com>
2024-08-26 19:48:33 +0700

    Add files via upload

---
+++ module2_2.py
@@ -0,0 +1,9 @@
+first = int(input())
+second = int(input())
+third = int(input())
+if first == second == third:
+    print(3)
+elif first != second != third != first:
+    print(0)
+else:
+    print(2)
