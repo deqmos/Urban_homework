2759025e85f823ba6aca63896cfcce566b4479a0
Yampile <102218673+deqmos@users.noreply.github.com>
2024-08-29 00:07:42 +0700

    rework

--- module2_3.py
+++ module2_3.py
@@ -1,18 +1,10 @@
-# Without using continue and break operators
-my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-index = 0
-while index < len(my_list) and my_list[index] >= 0:
-    print(my_list[index])
-    index += 1
-
-print()  # Cycle separation
-
-# With using continue and break operators
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 index = 0
 while index < len(my_list):
-    if my_list[index] >= 0:
-        print(my_list[index])
+    if my_list[index] == 0:
         index += 1
         continue
-    break
+    if my_list[index] < 0:
+        break
+    print(my_list[index])
+    index += 1
