84fe2ed924c775971349706244a65d116b8d2e8e
Yampile <102218673+deqmos@users.noreply.github.com>
2024-08-29 16:05:55 +0700

    Update module2_4.py

--- module2_4.py
+++ module2_4.py
@@ -5,11 +5,11 @@ def prime(n):
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
-for i in range(len(numbers)):
-    if prime(numbers[i]):
-        primes.append(numbers[i])
+for i in numbers:
+    if prime(i):
+        primes.append(i)
     else:
-        not_primes.append(numbers[i])
+        not_primes.append(i)
 
 print(f"Primes {primes}")
 print(f"Not Primes {not_primes}")
