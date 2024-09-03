905d7a02b8d243f136fb5446db8698d342c428ed
Yampile <102218673+deqmos@users.noreply.github.com>
2024-09-03 17:34:16 +0700

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,22 @@
+def count_calls():
+    global calls
+    calls += 1
+
+
+def string_info(string):
+    count_calls()
+    return tuple([len(string), string.upper(), string.lower()])
+
+
+def is_contains(string, list_to_search):
+    count_calls()
+    return string.lower() in map(lambda x: x.lower(), list_to_search)
+
+
+calls = 0
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
+
