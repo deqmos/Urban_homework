cbbc08c06603fe1233905d3f1e81478a0a399cd6
Yampile <102218673+deqmos@users.noreply.github.com>
2024-09-03 14:19:27 +0500

    Update module2_4.py
    Исправлено: числа меньше 2 не являются простыми или составными

--- module2_4.py
+++ module2_4.py
@@ -6,7 +6,9 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
 for i in numbers:
-    if prime(i):
+    if i < 2:
+        continue
+    elif prime(i):
         primes.append(i)
     else:
         not_primes.append(i)
