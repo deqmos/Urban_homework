a49bc892de8587982f91b54f5417036acb202dcd
Yampile <102218673+deqmos@users.noreply.github.com>
2024-09-08 20:13:10 +0700

    Add files via upload

---
+++ module_3_4.py
@@ -0,0 +1,12 @@
+def single_root_words(root_word, *other_words):
+    same_words = []
+    for i in other_words:
+        if i.lower() in root_word.lower() or root_word.lower() in i.lower():
+            same_words.append(i)
+    return same_words
+
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)
