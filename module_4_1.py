5c3648b770ea40c06bed5aa60410bda12c0546a1
Yampile <102218673+deqmos@users.noreply.github.com>
2024-10-07 12:35:40 +0500

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as fmd
+from true_math import divide as tmd
+
+result1 = fmd(69, 3)
+result2 = fmd(3, 0)
+result3 = tmd(49, 7)
+result4 = tmd(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
