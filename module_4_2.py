0b38f21e416a7897fb967ed7a6605274b0d8b260
Yampile <102218673+deqmos@users.noreply.github.com>
2024-10-07 13:24:10 +0500

    module_4_2.py

--- module_4_2.py
+++ module_4_2.py
@@ -3,5 +3,7 @@ def test_function():
         print("Я в области видимости функции test_function")
 
     inner_function()
+
+
 # inner_function - выведет ошибку т.к.  внутренняя функция выходит за область видимости программы
 test_function()
